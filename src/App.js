7ed050682370c577dabced7ed2a3f42b80bd1619
ggyaal <72740806+ggyaal@users.noreply.github.com>
2021-03-25 22:28:09 +0900

    commit3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
-import Home from './routes/Home';
-import About from './routes/About';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from '../components/Movie';
-import './Home.css';
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
 
 class Home extends React.Component {
   state = {
@@ -13,23 +13,23 @@ class Home extends React.Component {
       data: {
         data: { movies }
       }
-    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
     this.setState({ movies, isLoading: false });
-  }
-  
+  };
   componentDidMount() {
     this.getMovies();
   }
-
   render() {
     const { isLoading, movies } = this.state;
     return (
       <section className="container">
-        {isLoading
-        ? <div className="loader">
-          <span className="loader__text">Loading...</span>
-        </div>
-        : (
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
           <div className="movies">
             {movies.map(movie => (
               <Movie
